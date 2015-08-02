87f8c096f1279c97a390596462dcc8e1866bdc6f
Vahid Panjganj <panjganj@gmail.com>
2015-08-02 13:34:43 +0100

    Test case for player service

---
+++ test/spec/services/player.service-spec.js
@@ -0,0 +1,54 @@
+describe('player service', function () {
+    var player;
+
+
+    // Setup the mock service in an anonymous module.
+    beforeEach(function () {
+
+        module('app');
+
+        inject(function ($injector) {
+            var playerService = $injector.get('player');
+
+            player = new playerService('Tom','FirstRoom');
+        });
+
+    });
+
+
+    describe(', player object', function () {
+
+        it('should exist', function () {
+
+
+            expect(player).toBeDefined();
+
+        })
+        describe(', properties', function () {
+
+
+            it('should contain a name', function () {
+                expect(player.name).toBeDefined();
+            })
+
+            it('should always have a current location', function () {
+                expect(player.currentLocation).toBeDefined();
+                expect(player.currentLocation).not.toBeNull();
+                expect(player.currentLocation).toBeTruthy();
+
+            })
+        })
+
+        describe(', methods', function () {
+
+
+            it('should contain a move function', function () {
+                expect(player.move).toBeDefined();
+            })
+
+
+        })
+    });
+
+
+});
