d2a439ec617c4f323bbfc14f1400b10d6acf3a4c
Vahid Panjganj <panjganj@gmail.com>
2015-08-02 11:20:22 +0100

    Defining the basic player class. Writing test for the basic player class.

---
+++ app/services/player.service.js
@@ -0,0 +1,54 @@
+/*
+ * todo: add some comments here
+ */
+
+
+// player.service.js
+(function () {
+    'use strict';
+
+
+    angular
+        .module('app')
+        .factory('player', player);
+
+
+    player.$inject = [];
+
+
+    function player() {
+
+        //constructor
+        var player = function (name,startingLocation) {
+
+            //player properties
+            this.name = name;
+            this.currentLocation = startingLocation;// current location of the player, value should be a Room obj
+
+            //properties that may get handy in the future
+            //this.items = [] some sort of inventory
+            //
+
+        };
+
+
+        //player methods
+
+        //moving to next room with direction given
+        player.prototype.move = function (direction) {
+            var self = this;
+
+            //self.currentLocation = currentLocation.doorWay.getNextRoom();
+
+        }
+
+
+
+
+
+        return player;
+    }
+})();
+
+
+
